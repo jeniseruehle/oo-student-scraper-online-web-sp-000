3ea117ba4789b971b707025419b76f1d751cecd0
Jenise Ruehle <jela07@gmail.com>
2020-05-05 12:50:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,6 +4,9 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    students = []
+    html = Nokogiri::HTML(open(index_url))
+    html.css(".student-card").collect do |student|
 
   end
 
