e263d46bc7a170381c6254460492d8392846d444
Jenise Ruehle <jela07@gmail.com>
2020-05-05 13:06:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,14 +5,14 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     students = []
-    html = open(index_url)
-    index = Nokogiri::HTML(html)
-    index.css("div.student-card").collect do |student|
-      student_info = {}
-      student_info[:name] = student.css("h4.student-name").text
-      student_info[:location] = student.css("p.student-location").text 
-      profile_path = student.css("a").attribute("href").value
-      student_info[:profile_url] = './fixtures/student-site/' + profile_path
+    html = Nokogiri::HTML(open(index_url))
+    html.css(".student-card").collect do |student|
+      student_info = {
+        name: student.css("h4.student-name").text, 
+        location: student.css("p.student-location").text,
+        profile_url: "http://"
+      
+      }
       students << student_info
     end
     students   
